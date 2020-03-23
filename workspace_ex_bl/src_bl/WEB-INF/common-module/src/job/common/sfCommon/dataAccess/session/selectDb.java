824c3b79acac559469cfef0d14c12f3e2d9a11a2
D-Suzuki-yjp <62527669+D-Suzuki-yjp@users.noreply.github.com>
2020-03-23 16:47:48 +0900

    Add files via upload

---
+++ workspace_ex_bl/src_bl/WEB-INF/common-module/src/job/common/sfCommon/dataAccess/session/selectDb.java
@@ -0,0 +1,39 @@
+package job.common.sfCommon.dataAccess.session;
+
+import javax.enterprise.context.RequestScoped;
+
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * ========================== MODIFICATION HISTORY ==========================
+ * Release  Date       ID/Name                   Comment
+ * --------------------------------------------------------------------------
+ * R0.01.01 2020/03/1 30042453/D.Suzuki         初版
+ * [END OF MODIFICATION HISTORY]
+ * ==========================================================================
+ *
+ * selectDbAcssece 接続先DB選択クラス
+ * @author D.Suzuki
+ */
+public class selectDb {
+
+	/**
+	 * 接続先DB選択処理
+	 * @param dbName 接続先DB名
+	 * @return SqlSession
+	 */
+	@RequestScoped
+	public static SqlSession dbAcssece(String dbName) {
+
+		SqlSession session = null;
+		/** TODO DB名のプロパティ化*/
+		if(dbName.equals("looponex_core")){
+			session = looponexcoreSqlSessionFactory.openSession();
+		} else if(dbName.equals("nhlng")){
+			session = nhlngSqlSessionFactory.openSession();
+		}
+
+		return session;
+	}
+
+}
