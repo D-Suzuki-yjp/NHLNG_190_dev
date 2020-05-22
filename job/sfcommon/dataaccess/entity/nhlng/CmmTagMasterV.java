2b2fa630c6c818adc08798d23dfce7ad876c7fd5
D-Suzuki-yjp <62527669+D-Suzuki-yjp@users.noreply.github.com>
2020-05-22 18:14:53 +0900

    Add files via upload

---
+++ job/sfcommon/dataaccess/entity/nhlng/CmmTagMasterV.java
@@ -0,0 +1,288 @@
+package job.sfcommon.dataaccess.entity.nhlng;
+
+/**
+ * ========================== MODIFICATION HISTORY ==========================
+ * Release Date ID/Name Comment
+ * --------------------------------------------------------------------------
+ * R0.01.01 2020/04/01 30042453/D.Suzuki 初版 [END OF MODIFICATION HISTORY]
+ * ==========================================================================
+ *
+ * CmmTagMasterV NHタグマスタ(ビュー)entity ジェネレータがviewからは生成しないので手動作成
+ *
+ * @author D.Suzuki
+ */
+import javax.enterprise.context.Dependent;
+
+@Dependent
+public class CmmTagMasterV {
+
+	private String tagNo;
+
+	private String lcode;
+
+	private String comment1;
+
+	private String unitComputer;
+
+	private Integer dp;
+
+	private Float upperValue;
+
+	private Float lowerValue;
+
+	private Integer tagKind;
+
+	private String ioKind;
+
+	private String constDiv;
+
+	private String repoDiv;
+
+	private String repoCalcDiv;
+
+	private Integer odbMinSndFlag;
+
+	private Integer odbHourSndFlag;
+
+	private Integer odbDaySndFlag;
+
+	private Integer roundFlag;
+
+	private String remTagNo;
+
+	private Integer scrUpdateFlag;
+
+	private String tagLogicName1;
+
+	private String tagLogicName2;
+
+	private String tagLogicName3;
+
+	private String tagLogicName4;
+
+	private String currentDataFlag;
+
+	private String hourlyDataFlag;
+
+	private String dailyDataFlag;
+
+	private String monthlyDataFlag;
+
+	private String closeKind;
+
+	public String getTagNo() {
+		return tagNo;
+	}
+
+	public void setTagNo(String tagNo) {
+		this.tagNo = tagNo;
+	}
+
+	public String getLcode() {
+		return lcode;
+	}
+
+	public void setLcode(String lcode) {
+		this.lcode = lcode;
+	}
+
+	public String getComment1() {
+		return comment1;
+	}
+
+	public void setComment1(String comment1) {
+		this.comment1 = comment1;
+	}
+
+	public String getUnitComputer() {
+		return unitComputer;
+	}
+
+	public void setUnitComputer(String unitComputer) {
+		this.unitComputer = unitComputer;
+	}
+
+	public Integer getDp() {
+		return dp;
+	}
+
+	public void setDp(Integer dp) {
+		this.dp = dp;
+	}
+
+	public Float getUpperValue() {
+		return upperValue;
+	}
+
+	public void setUpperValue(Float upperValue) {
+		this.upperValue = upperValue;
+	}
+
+	public Float getLowerValue() {
+		return lowerValue;
+	}
+
+	public void setLowerValue(Float lowerValue) {
+		this.lowerValue = lowerValue;
+	}
+
+	public Integer getTagKind() {
+		return tagKind;
+	}
+
+	public void setTagKind(Integer tagKind) {
+		this.tagKind = tagKind;
+	}
+
+	public String getIoKind() {
+		return ioKind;
+	}
+
+	public void setIoKind(String ioKind) {
+		this.ioKind = ioKind;
+	}
+
+	public String getConstDiv() {
+		return constDiv;
+	}
+
+	public void setConstDiv(String constDiv) {
+		this.constDiv = constDiv;
+	}
+
+	public String getRepoDiv() {
+		return repoDiv;
+	}
+
+	public void setRepoDiv(String repoDiv) {
+		this.repoDiv = repoDiv;
+	}
+
+	public String getRepoCalcDiv() {
+		return repoCalcDiv;
+	}
+
+	public void setRepoCalcDiv(String repoCalcDiv) {
+		this.repoCalcDiv = repoCalcDiv;
+	}
+
+	public Integer getOdbMinSndFlag() {
+		return odbMinSndFlag;
+	}
+
+	public void setOdbMinSndFlag(Integer odbMinSndFlag) {
+		this.odbMinSndFlag = odbMinSndFlag;
+	}
+
+	public Integer getOdbHourSndFlag() {
+		return odbHourSndFlag;
+	}
+
+	public void setOdbHourSndFlag(Integer odbHourSndFlag) {
+		this.odbHourSndFlag = odbHourSndFlag;
+	}
+
+	public Integer getOdbDaySndFlag() {
+		return odbDaySndFlag;
+	}
+
+	public void setOdbDaySndFlag(Integer odbDaySndFlag) {
+		this.odbDaySndFlag = odbDaySndFlag;
+	}
+
+	public Integer getRoundFlag() {
+		return roundFlag;
+	}
+
+	public void setRoundFlag(Integer roundFlag) {
+		this.roundFlag = roundFlag;
+	}
+
+	public String getRemTagNo() {
+		return remTagNo;
+	}
+
+	public void setRemTagNo(String remTagNo) {
+		this.remTagNo = remTagNo;
+	}
+
+	public Integer getScrUpdateFlag() {
+		return scrUpdateFlag;
+	}
+
+	public void setScrUpdateFlag(Integer scrUpdateFlag) {
+		this.scrUpdateFlag = scrUpdateFlag;
+	}
+
+	public String getTagLogicName1() {
+		return tagLogicName1;
+	}
+
+	public void setTagLogicName1(String tagLogicName1) {
+		this.tagLogicName1 = tagLogicName1;
+	}
+
+	public String getTagLogicName2() {
+		return tagLogicName2;
+	}
+
+	public void setTagLogicName2(String tagLogicName2) {
+		this.tagLogicName2 = tagLogicName2;
+	}
+
+	public String getTagLogicName3() {
+		return tagLogicName3;
+	}
+
+	public void setTagLogicName3(String tagLogicName3) {
+		this.tagLogicName3 = tagLogicName3;
+	}
+
+	public String getTagLogicName4() {
+		return tagLogicName4;
+	}
+
+	public void setTagLogicName4(String currentDataFlag) {
+		this.currentDataFlag = currentDataFlag;
+	}
+
+	public String getCurrentDataFlag() {
+		return currentDataFlag;
+	}
+
+	public void setCurrentDataFlag(String currentDataFlag) {
+		this.currentDataFlag = currentDataFlag;
+	}
+
+	public String getHourlyDataFlag() {
+		return hourlyDataFlag;
+	}
+
+	public void setHourlyDataFlag(String hourlyDataFlag) {
+        this.hourlyDataFlag = hourlyDataFlag;
+    }
+
+	public String getDailyDataFlag() {
+		return dailyDataFlag;
+	}
+
+	public void setDailyDataFlag(String dailyDataFlag) {
+		this.dailyDataFlag = dailyDataFlag;
+	}
+
+	public String getMonthlyDataFlag() {
+		return monthlyDataFlag;
+	}
+
+	public void setMonthlyDataFlag(String monthlyDataFlag) {
+		this.monthlyDataFlag = monthlyDataFlag;
+	}
+
+	public String getCloseKind() {
+		return closeKind;
+	}
+
+	public void setCloseKind(String closeKind) {
+		this.closeKind = closeKind;
+	}
+}
