bf5009e7e98f65fc92aee5cf3ea6b683010dbc1b
D-Suzuki-yjp <62527669+D-Suzuki-yjp@users.noreply.github.com>
2020-05-22 19:04:20 +0900

    Add files via upload

---
+++ job/sfcommon/dto/dto/NhTagDataDto.java
@@ -0,0 +1,72 @@
+package job.sfcommon.dto;
+
+import java.util.Date;
+
+/**
+ * ========================== MODIFICATION HISTORY ==========================
+ * Release Date ID/Name Comment
+ * --------------------------------------------------------------------------
+ * R0.01.01 2020/04/7 30042453/D.Suzuki 初版 [END OF MODIFICATION HISTORY]
+ * ==========================================================================
+ *
+ * CmtCloseHourDto NH日締データDTO<br>
+ * String tagNo タグNo<br>
+ * CurrentTrend currentTrend トレンドデータ<br>
+ * String CurrentTrend.value 値<br>
+ * String CurrentTrend.collectDate 収集日時<br>
+ *
+ * @author D.Suzuki
+ */
+public class NhTagDataDto{
+    /**
+     * タグNo
+     */
+    private String tagNo;
+
+    /**
+     * タグ値(JobDb)
+     */
+    private String tagData;
+
+    /**
+     * 品質コード
+     */
+    private Integer qltyCd;
+
+    /**
+     * 収集日時(JobDb)
+     */
+    private Date collDTime;
+
+    public String getTagNo() {
+        return tagNo;
+    }
+
+    public void setTagNo(String tagNo) {
+        this.tagNo = tagNo;
+    }
+
+    public String getTagData() {
+        return tagData;
+    }
+
+    public void setTagData(String tagData) {
+        this.tagData = tagData;
+    }
+
+    public Integer getQltyCd() {
+        return qltyCd;
+    }
+
+    public void setQltyCd(Integer qltyCd) {
+        this.qltyCd = qltyCd;
+    }
+
+    public Date getCollDTime() {
+        return collDTime;
+    }
+
+    public void setCollDTime(Date collDTime) {
+        this.collDTime = collDTime;
+    }
+}
